608e6eb3d092b910fb93fa578eb2afa81566ffb4
vineetbadoni <vineetbadoni@gmail.com>
2021-01-04 20:20:37 +0530

    Tuple Examples

--- practice/datascience/python/basics/TupleExample.py
+++ practice/datascience/python/basics/TupleExample.py
@@ -69,4 +69,65 @@ t = ("apple", "banana", "cherry")
 del t
 # print(t) #this will raise an error because the tuple no longer exists
 
+# Unpack Tuple
+fruits = ("apple", "banana", "cherry")
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+# Wild card unpacking
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+# red will have cherry, strawberry , raspberry
+print(red)
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+
+(green, *tropic, red) = fruits
+
+print(green)
+# tropic will have mango, papaya , pineapple
+print(tropic)
+print(red)
+
+# Confused wildcard
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+# Gives syntax error
+# (green, *tropic, *red) = fruits
+
+######################Loop###############
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+for x in fruits:
+  print (x)
+
+# While loop
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+i = 0
+while (i<len(fruits)):
+  print(fruits[i])
+  i+=1
+
+# Join tuples
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# Multiply tuple
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+# appends same tuple , same as fruits + fruits
+mytuple = fruits * 2
+print(mytuple)
+
 
