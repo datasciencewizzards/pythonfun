b647278910c4d32de6f9b4bfe15ca8ea5da5346a
vineetbadoni <vineetbadoni@gmail.com>
2021-01-12 19:24:40 +0530

    Classes , Objects and Inheritance

--- practice/datascience/pandas/PandasPractice_changedataops.py
+++ practice/datascience/pandas/PandasPractice_changedataops.py
@@ -12,17 +12,16 @@ print(df.columns)
 
 # Create an additional column called "Total" which is the sum of
 # 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'
-df['Total'] = df['HP']+df['Attack']+df[ 'Defense']+df[ 'Sp. Atk']+df[ 'Sp. Def']+df[ 'Speed']
+df['Total'] = df['HP'] + df['Attack'] + df['Defense'] + df['Sp. Atk'] + df['Sp. Def'] + df['Speed']
 
-print("After creating one more column Total \n",df.head(5))
+print("After creating one more column Total \n" , df.head(5))
 
-print("Statistics after total\n",df.describe())
-
+print("Statistics after total\n" , df.describe())
 
 # Drop column Type 1
 
-#df = df.drop(columns=['Type 1'])
-print("DataFrame after dropping the column 'Total 1'\n",df)
+# df = df.drop(columns=['Type 1'])
+print("DataFrame after dropping the column 'Total 1'\n" , df)
 
 # Another way of doing the same
 # df['Total'] = df['HP']+df['Attack']+df[ 'Defense']+df[ 'Sp. Atk']+df[ 'Sp. Def']+df[ 'Speed']
@@ -31,5 +30,12 @@ print("DataFrame after dropping the column 'Total 1'\n",df)
 # (2) Sum up all the columns from (1) df.loc.sum
 # (3) Put back the sum in the df['Total']
 
-df['Total'] = df.iloc[:,4:10].sum(axis=1)
-print("After creating one more column Total \n",df.head(5))
+df['Total'] = df.iloc[: , 4:10].sum(axis=1)
+print("After creating one more column Total \n" , df.head(5))
+
+# Moving total column from the last to middle somewhere
+cols = list(df.columns)
+df = df[cols[0:4] + [cols[-1]] + cols[4:12]]
+print("Dataframe after moving the Total in the middle\n" , df.head(5))
+
+df.to_csv("modified.csv")
--- practice/datascience/pandas/PandasSum.py
+++ practice/datascience/pandas/PandasSum.py
@@ -5,6 +5,25 @@ df = pd.read_csv("data.csv")
 
 print("Before adding the values :: \n",df)
 
+# Axis is unclear
 df['Total'] = df.iloc[:,4:10].sum(axis=1)
 
 print("After adding the values :: \n",df)
+
+# More tryouts
+data = {'Month': ['Jan ','Feb ','Mar ','Apr ','May ','Jun '],
+        'Bill Commission': [1500,2200,3500,1800,3000,2800],
+        'Maria Commission': [3200,4100,2500,3000,4700,3400],
+        'Jack Commission': [1700,3100,3300,2700,2400,3100]
+        }
+
+df = pd.DataFrame(data,columns=['Month','Bill Commission','Maria Commission','Jack Commission'])
+print (df)
+
+sum_column = df.sum(axis=0)
+print (sum_column)
+
+# Axis if we try to store , it says not a NAN
+df['sum_column'] = df.sum(axis=1)
+print (df)
+
